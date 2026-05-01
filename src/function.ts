5f5ae62b2bbd8a101392d103b70faaafce0068bf
alhaj-cmd <alhazsarkar5523@gmail.com>
2026-05-01 09:35:58 +0600

    method function

--- src/function.ts
+++ src/function.ts
@@ -1,3 +1,16 @@
 function add(num1:number, num2:number):number{
     return(num1+num2)
 }
+
+// object => function => method
+
+const poorUser ={
+    name: 'sujon',
+    balance: 0,
+    addBalance(value:number): number{
+        const totalBalance = this.balance + value;
+        return totalBalance;
+    }
+
+}
+poorUser.addBalance(200)
